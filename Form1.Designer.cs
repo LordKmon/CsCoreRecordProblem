cb70522601989ab7142a289399bd3d20619a5401
LordKmon <demonkmon@gmail.com>
2021-08-25 19:12:39 +0300

    Test-Project added
    Test-Project added

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WriteCSCoreProblem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_StartRecord = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.l_Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_StartRecord
+            // 
+            this.btn_StartRecord.Location = new System.Drawing.Point(123, 73);
+            this.btn_StartRecord.Name = "btn_StartRecord";
+            this.btn_StartRecord.Size = new System.Drawing.Size(137, 61);
+            this.btn_StartRecord.TabIndex = 0;
+            this.btn_StartRecord.Text = "Start record!";
+            this.btn_StartRecord.UseVisualStyleBackColor = true;
+            this.btn_StartRecord.Click += new System.EventHandler(this.btn_StartRecord_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(123, 189);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(137, 66);
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.Text = "Stop record";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // l_Status
+            // 
+            this.l_Status.AutoSize = true;
+            this.l_Status.Location = new System.Drawing.Point(302, 107);
+            this.l_Status.Name = "l_Status";
+            this.l_Status.Size = new System.Drawing.Size(16, 13);
+            this.l_Status.TabIndex = 2;
+            this.l_Status.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 369);
+            this.Controls.Add(this.l_Status);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_StartRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_StartRecord;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Label l_Status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using CSCore;
+using CSCore.Codecs.WAV;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.Streams;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WriteCSCoreProblem
+{
+    public partial class Form1 : Form
+    {
+        private WasapiCapture m_SoundKeeper;
+        private IWriteable m_Writer;
+        private IWaveSource m_FinalSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_StartRecord_Click(object sender, EventArgs event_args)
+        {
+            //Find and set Device (microphone)
+            MMDevice selected_device = null;
+            using (var deviceEnumerator = new MMDeviceEnumerator())
+            using (var deviceCollection = deviceEnumerator.EnumAudioEndpoints(DataFlow.Capture, DeviceState.Active))
+            {
+                selected_device = deviceCollection[0];
+            }
+
+            // Format that I needed
+            WaveFormat need_wave_format = new WaveFormat(16000, 16, 1, AudioEncoding.Pcm);
+
+            m_SoundKeeper = new WasapiCapture();
+            m_SoundKeeper.Device = selected_device;
+            m_SoundKeeper.Initialize();
+            var soundInSource = new SoundInSource(m_SoundKeeper);
+
+            var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+            m_FinalSource = singleBlockNotificationStream.ToWaveSource();
+
+            m_Writer = new WaveWriter("output.wav", m_FinalSource.WaveFormat);
+
+            byte[] buffer = new byte[m_FinalSource.WaveFormat.BytesPerSecond / 2];
+            soundInSource.DataAvailable += (s, e) =>
+            {
+                int read;
+                while ((read = m_FinalSource.Read(buffer, 0, buffer.Length)) > 0)
+                    m_Writer.Write(buffer, 0, read);
+            };
+
+            l_Status.Text = "RECORD !!!";
+            m_SoundKeeper.Start();
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            if (m_SoundKeeper == null)
+                return;
+            
+            m_SoundKeeper.Stop();
+            m_SoundKeeper.Dispose();
+            m_SoundKeeper = null;
+            m_FinalSource.Dispose();
+
+            if (m_Writer is IDisposable)
+                ((IDisposable)m_Writer).Dispose();
+
+            l_Status.Text = "...";
+        }
+    }
+}
+
